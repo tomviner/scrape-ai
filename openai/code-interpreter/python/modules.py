0cbb1b3691642bd67fc5f955b6d7f85b306bf711
Tom Viner <tom@viner.tv>
2023-07-11 07:56:42 +0100

    improve modules prompt/code and sorting

--- openai/code-interpreter/python/modules.py
+++ openai/code-interpreter/python/modules.py
@@ -1,11 +1,35 @@
+# To continue our previous chat. Here's your last message in full:
+
+# Assistant: """
+# I understand that you'd like to run help("modules") and store the sorted module names in a file called modules.txt while ignoring the stderr.
+
+# Please use the following Python code:
+# ```python
+import os
+import io
 import sys
-from contextlib import redirect_stdout
-log = open("/mnt/data/modules.txt", "w")
+from contextlib import redirect_stdout, redirect_stderr
+
+log = io.StringIO()
+devnull = open(os.devnull, 'w')
 with redirect_stdout(log):
-    help("modules")
+    with redirect_stderr(devnull):
+        help("modules")
 
-# now rewrite the file to
-# - remove the intro and help comments from the file
-# - split and sort the module list
+devnull.close()
 
-'/mnt/data/modules.txt'
+paragraphs = log.getvalue().split('\n\n')
+module_text = paragraphs[1].strip()
+module_names = module_text.split()
+module_names.sort()  # Sort the list of module names
+data = '\n'.join(module_names)
+
+with open("/mnt/data/modules.txt", 'w') as file:
+    file.write(data)
+
+print(f'Written {len(data)} bytes to /mnt/data/modules.txt')
+# ```
+
+# This code will capture the output of help("modules"), sort the module names alphabetically, and write the sorted list to a file named modules.txt.
+
+# # User: so let's do that please :-)
